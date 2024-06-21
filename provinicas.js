fb201f9593a6db96320e69e2ad300980c1e8db42
Juan <alonso.jad@gmail.com>
2024-06-21 08:57:41 +0200

    ejercicios 4

---
+++ provinicas.js
@@ -0,0 +1,87 @@
+const comunidadesProvincias = {
+    "Andalucía": [
+        "Almería",
+        "Cádiz",
+        "Córdoba",
+        "Granada",
+        "Huelva",
+        "Jaén",
+        "Málaga",
+        "Sevilla"
+    ],
+    "Aragón": [
+        "Huesca",
+        "Teruel",
+        "Zaragoza"
+    ],
+    "Asturias": [
+        "Asturias"
+    ],
+    "Baleares": [
+        "Baleares"
+    ],
+    "Canarias": [
+        "Las Palmas",
+        "Santa Cruz de Tenerife"
+    ],
+    "Cantabria": [
+        "Cantabria"
+    ],
+    "Castilla y León": [
+        "Ávila",
+        "Burgos",
+        "León",
+        "Palencia",
+        "Salamanca",
+        "Segovia",
+        "Soria",
+        "Valladolid",
+        "Zamora"
+    ],
+    "Castilla-La Mancha": [
+        "Albacete",
+        "Ciudad Real",
+        "Cuenca",
+        "Guadalajara",
+        "Toledo"
+    ],
+    "Cataluña": [
+        "Barcelona",
+        "Girona",
+        "Lleida",
+        "Tarragona"
+    ],
+    "Extremadura": [
+        "Badajoz",
+        "Cáceres"
+    ],
+    "Galicia": [
+        "A Coruña",
+        "Lugo",
+        "Ourense",
+        "Pontevedra"
+    ],
+    "Madrid": [
+        "Madrid"
+    ],
+    "Murcia": [
+        "Murcia"
+    ],
+    "Navarra": [
+        "Navarra"
+    ],
+    "País Vasco": [
+        "Álava",
+        "Gipuzkoa",
+        "Bizkaia"
+    ],
+    "La Rioja": [
+        "La Rioja"
+    ],
+    "Ceuta": [
+        "Ceuta"
+    ],
+    "Melilla": [
+        "Melilla"
+    ]
+};
